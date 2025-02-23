4c63187b803cd0b62a511462ec26f01a25da5e5e
Eugene Polyakov <74543647+teoretik-eugene@users.noreply.github.com>
2025-02-23 20:05:25 +0300

    Update server.c

--- task 2.1/server.c
+++ task 2.1/server.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
-
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -13,111 +11,213 @@
 #define CONFIG_FILE "config"
 
 char* reverse_string(char *str);
+int read_socket_type_config();
 char read_key_config();
 char* encrypt(char *input);
 
-int main () {
-
-    char server_message[256] = "You have reached the server!";
+int main() {
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     int server_fd, new_socket;
     char buffer[BUFFER_SIZE] = {0};
 
-    // Создание сокета
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("SOCKET ERROR");
+    // Чтение типа сокета из конфигурационного файла
+    int type = read_socket_type_config();
+    if (type == -1) {
+        perror("config socket error");
         exit(EXIT_FAILURE);
     }
 
-    // Настройка адреса сервера
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; // Принимать соединения на все интерфейсы
-    address.sin_port = htons(PORT);
+    if (type == SOCK_STREAM) { // TCP
+        printf("Using TCP\n");
 
-    // Привязка сокета к адресу
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
+        // Создание сокета
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+            perror("SOCKET ERROR");
+            exit(EXIT_FAILURE);
+        }
 
-    // Ожидание подключений
-    if (listen(server_fd, 3) < 0) {
-        perror("listen failed\n");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
+        // Настройка адреса сервера
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY; // Принимать соединения на все интерфейсы
+        address.sin_port = htons(PORT);
 
-    printf("Server is listening on port %d...\n", PORT);
-    while (1) {
-        // Принятие подключения
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");
+        // Привязка сокета к адресу
+        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+            perror("bind failed");
             close(server_fd);
             exit(EXIT_FAILURE);
         }
 
-        // Чтение данных
-        int valread = read(new_socket, buffer, BUFFER_SIZE);
-        printf("Input message: %s\n", buffer);
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0';
+        // Ожидание подключений
+        if (listen(server_fd, 3) < 0) {
+            perror("listen failed");
+            close(server_fd);
+            exit(EXIT_FAILURE);
         }
-        // Декодируем строку
-        char* decrypted = encrypt(buffer);
-        // Разворачиваем
-        char *reversed = reverse_string(decrypted);
-        printf("response: %s\n", reversed);
-        // Кодируем обратно
-        char *encrypted_response = encrypt(reversed);
-        send(new_socket, encrypted_response, strlen(encrypted_response), 0);
 
-        // Освобождение памяти и очистка буфера
-        free(encrypted_response);
-        free(reversed);
-        free(decrypted);
-        memset(buffer, 0, BUFFER_SIZE);
+        printf("Server is listening on port %d...\n", PORT);
+
+        while (1) {
+            // Принятие подключения
+            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+                perror("accept");
+                continue;
+            }
+
+            // Чтение данных
+            int valread = read(new_socket, buffer, BUFFER_SIZE);
+            if (valread <= 0) {
+                perror("read failed");
+                close(new_socket);
+                continue;
+            }
+
+            printf("Input message: %s\n", buffer);
+
+            // Удаление символа новой строки, если он есть
+            size_t len = strlen(buffer);
+            if (len > 0 && buffer[len - 1] == '\n') {
+                buffer[len - 1] = '\0';
+            }
+
+            // Декодируем строку
+            char* decrypted = encrypt(buffer);
+
+            // Разворачиваем строку
+            char *reversed = reverse_string(decrypted);
+
+            printf("Reversed message: %s\n", reversed);
+
+            // Кодируем обратно
+            char *encrypted_response = encrypt(reversed);
+
+            // Отправка ответа
+            send(new_socket, encrypted_response, strlen(encrypted_response), 0);
+
+            // Освобождение памяти
+            free(encrypted_response);
+            free(reversed);
+            free(decrypted);
+
+            memset(buffer, 0, BUFFER_SIZE);
+
+            // Закрытие сокета клиента
+            close(new_socket);
+
+            printf("Client disconnected.\n");
+        }
+
+        // Закрытие серверного сокета
+        close(server_fd);
+
+    } else if (type == SOCK_DGRAM) { // UDP
+        printf("Using UDP\n");
+
+        // Создание сокета
+        if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            perror("SOCKET ERROR");
+            exit(EXIT_FAILURE);
+        }
+
+        // Настройка адреса сервера
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY; // Принимать соединения на все интерфейсы
+        address.sin_port = htons(PORT);
+
+        // Привязка сокета к адресу
+        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+            perror("bind failed");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Server is listening on port %d...\n", PORT);
+
+        while (1) {
+            struct sockaddr_in client_addr;
+            socklen_t client_addrlen = sizeof(client_addr);
+
+            // Получение данных
+            int valread = recvfrom(server_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_addrlen);
+            if (valread <= 0) {
+                perror("recvfrom failed");
+                continue;
+            }
+
+            printf("Received message: %s\n", buffer);
+
+            // Удаление символа новой строки, если он есть
+            size_t len = strlen(buffer);
+            if (len > 0 && buffer[len - 1] == '\n') {
+                buffer[len - 1] = '\0';
+            }
+
+            // Декодируем строку
+            char* decrypted = encrypt(buffer);
+
+            // Разворачиваем строку
+            char *reversed = reverse_string(decrypted);
+
+            printf("Reversed message: %s\n", reversed);
+
+            // Кодируем обратно
+            char *encrypted_response = encrypt(reversed);
+
+            // Отправка ответа
+            sendto(server_fd, encrypted_response, strlen(encrypted_response), 0, (struct sockaddr *)&client_addr, client_addrlen);
+
+            // Освобождение памяти
+            free(encrypted_response);
+            free(reversed);
+            free(decrypted);
+
+            memset(buffer, 0, BUFFER_SIZE);
+        }
+
+        // Закрытие серверного сокета
+        close(server_fd);
     }
-
-    // Закрытие сокетов
-    close(new_socket);
-    close(server_fd);
 
     return 0;
 }
 
 char* reverse_string(char *str) {
     int len = strlen(str);
-    char *reversed = (char*)malloc(strlen(str) + 1);
+    char *reversed = (char*)malloc(len + 1);
     if (reversed == NULL) {
-        perror("mem err");
+        perror("malloc failed");
         exit(EXIT_FAILURE);
     }
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++) {
         reversed[i] = str[len - (i + 1)];
+    }
     reversed[len] = '\0';
     return reversed;
 }
 
-char read_config() {
+int read_socket_type_config() {
     FILE *file = fopen(CONFIG_FILE, "r");
     if (file == NULL) {
         perror("Failed to open config file");
         exit(EXIT_FAILURE);
     }
 
-    char key[10];
-    if (fscanf(file, "XOR_KEY=%s", key) != 1) {
-        printf(stderr, "Invalid config file format\n");
-        fclose(file);
-        exit(EXIT_FAILURE);
+    char line[100];
+    while (fgets(line, sizeof(line), file)) {
+        if (sscanf(line, "SOCKET_TYPE=%s", line) == 1) {
+            fclose(file);
+            if (strcmp(line, "TCP") == 0) {
+                return SOCK_STREAM;
+            } else if (strcmp(line, "UDP") == 0) {
+                return SOCK_DGRAM;
+            }
+        }
     }
 
+    fprintf(stderr, "SOCKET_TYPE not found in config file\n");
     fclose(file);
-
-    return (char)strtol(key, NULL, 0);
+    exit(EXIT_FAILURE);
 }
 
 char read_key_config() {
@@ -127,7 +227,7 @@ char read_key_config() {
         exit(EXIT_FAILURE);
     }
 
-    char line[BUFFER_SIZE];
+    char line[100];
     char key[10] = {0};
 
     // Чтение строки из файла
@@ -144,17 +244,15 @@ char read_key_config() {
         fprintf(stderr, "Invalid config file format. Expected 'XOR_KEY=value'\n");
         exit(EXIT_FAILURE);
     }
-    //fprintf("key: %s\n", key);
 
     // Преобразование строки в число (например, "0xAA" -> 0xAA)
+    printf("%s\n", key);
     return (char)strtol(key, NULL, 0);
 }
 
-
-// Функция для шифрования строки
 char* encrypt(char *input) {
     char key = read_key_config();
-    printf("Using XOR key: \n", (unsigned char)key);
+    printf("Using XOR key: 0x%02X\n", (unsigned char)key);
 
     size_t len = strlen(input);
     char *output = (char*)malloc(len + 1); // +1 для нуль-терминатора
